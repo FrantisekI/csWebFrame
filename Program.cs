61e87ae54f9a02e298885e57c82075aca73a5431
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2025-04-09 14:21:14 +0200

    http listener

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SimpleListenerExample(new string[] { "http://localhost:8060/" });
+        }
+
+        // This example requires the System and System.Net namespaces.
+        public static void SimpleListenerExample(string[] prefixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            while (listener.IsListening)
+            {
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                Console.WriteLine($"Received request: {request.HttpMethod} {request.Url}");
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world! <img src='vobrazek'>bob</img></BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
